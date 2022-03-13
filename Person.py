db44467101a40f0fa9e471786ce222692f54775f
Jibran Absarulislam <jga26@njit.edu>
2022-03-12 23:37:41 -0500

    file cleanup

--- Person.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, name):
-        self.name = name
-        self.notify_list = []
-
-    def notify(self, any_username):
-        return any_username in self.notify_list
