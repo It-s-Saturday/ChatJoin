809bb5c9b2b7e1a5bf45e251e7c884cdeeb02a97
Jibran Absarulislam <jga26@njit.edu>
2022-03-12 20:09:22 -0500

    stash

--- notify.py
+++ notify.py
@@ -2,11 +2,12 @@
 
 from mongo_files.Connect_Cluster import Connect_Cluster
 
+class Notify():
+    def __init__(self, target, server_id=None) -> None:
+        self.target = target
+        self.server_id = server_id
 
-def __init__(self, target, server_id=None) -> None:
-    self.target = target
-    self.server_id = server_id
+    def get_targets(self):
+        client = Connect_Cluster(self.server_id)
+        client.add_user_to_target
 
-def get_targets(self):
-    client = Connect_Cluster(self.server_id)
-    
